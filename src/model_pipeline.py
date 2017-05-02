be927e93f17d078ee21c75b6228f31bd60b9584d
cat chen <xi.catherine@gmail.com>
2017-05-02 16:00:26 -0700

    updating model pipeline.py

--- src/model_pipeline.py
+++ src/model_pipeline.py
@@ -10,56 +10,68 @@ from sklearn import svm
 
 class ModelPipeline(object):
 
-    def __init__(self, json=None, json_logistic=None, ylabel=None):
-        self.json = json
-        self.json_logistic = json_logistic
-        self.ylabel = ylabel
+    def __init__(self, df=None):
+        self.df = df
 
-    def transform_json_df(self):
-        cleaned_engineered_df = pd.read_json(self.json)
-        return cleaned_engineered_df
-
-    def get_baseline(self, cleaned_engineered_df=None, ylabel=None):
-        y_true = cleaned_engineered_df[ylabel]
-        cleaned_engineered_df['yhat_baseline'] = 1
-        y_pred = cleaned_engineered_df['yhat_baseline']
+    def get_baseline_scores(self, df, ylabel):
+        '''
+        inputs: cleaned/engineered df and ylabel name
+        outputs: list of baseline scores (precision, recall, f1)
+        '''
+        y_true = df[ylabel]
+        df['yhat_baseline'] = 1
+        y_pred = df['yhat_baseline']
         baseline_precision = metrics.precision_score(y_true, y_pred)
         baseline_recall = metrics.recall_score(y_true, y_pred)
         baseline_f1 = metrics.f1_score(y_true, y_pred)
-        return([('baseline_precision:', baseline_precision),
-               ('baseline_recall:', baseline_recall),
-               ('baseline_f1:', baseline_f1)])
+        return([('baseline_precision', baseline_precision),
+               ('baseline_recall', baseline_recall),
+               ('baseline_f1', baseline_f1)])
 
-    def get_model_scores(self, cleaned_engineered_df=None, ylabel=None):
+    def run_logistic_separately(self, df, list_vars_to_drop, ylabel):
+        for item in list_vars_to_drop:
+            df.pop(item)
+        df_logistic = df
+        y = df_logistic.pop(ylabel)
+        X = df_logistic
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            random_state=42)
+        logistic = linear_model.LogisticRegression(penalty='l1')
+        logistic.fit(X_train, y_train)
+        log_score = cross_val_score(logistic, X_train, y_train,
+                                    cv=5, scoring='f1_macro')
+        return(zip('logistic', log_score))
+
+    def split_df(self, df, ylabel):
+        # create X, y, then do train_test_split
+        y = df.pop(ylabel)
+        X = df
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            random_state=42)
+        return X_train, X_test, y_train, y_test
+
+    def get_othermodels_scores(self, df, ylabel):
         '''
-        input: cleaned/engineered json file
+        input: cleaned/engineered df
         output: return f1_macro scores from models
         '''
-        # models to run
-        # logistic = linear_model.LogisticRegression(penalty='l1')
+        # set models to run in pipeline
         sgd = linear_model.SGDClassifier(loss="log")
         svm = svm.LinearSVC()
         randomforest = ensemble.RandomForestClassifier()
-
         # save down sequence of models to run for future reference
         model_sequence = ['sgd', 'svm', 'randomforest']
-
         # define a pipeline
         pipe = Pipeline([('sgd', sgd),
                         ('svm', svm),
                         ('randomforest', randomforest)])
-
-        # create X, y, then do train_test_split
-        y = cleaned_engineered_df.pop(ylabel)
-        X = cleaned_engineered_df
-        X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                            random_state=42)
-
+        # call in split df
+        X_train, X_test, y_train, y_test = self.split_df(df, ylabel)
         # fit models with train set
         pipe.fit(X_train, y_train)
         scores = cross_val_score(pipe, X_train, y_train,
                                  cv=5, scoring='f1_macro')
         return(zip(model_sequence, scores))
 
-    def parameter_tuning(self, df_logistic=None, df=None, ylabel=None):
+    def parameter_tuning(self, df, ylabel):
         pass
