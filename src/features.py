d86bfe8b299f8ab365ef63de7efc79f084602a74
rita.glv@gmail.com <johndoe@example.com>
2017-05-02 13:22:44 -0700

    added cleaning and feature functions

---
+++ src/features.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+def impute_vals(df,col_name,val):
+    '''
+    Impute defined value val into col_name. Operates over one column at a time
+    To run outside of class
+    '''
+    df[col_name] = df[col_name].fillna(val)
+    return df
+
+def minimal_df(df,col_names, to_drop = False):
+    '''
+    input: list of col_names
+    Returns a dataframe with only col_names leff, or drops col_names from the datafram if to_drop == True
+    To run outside of class
+    '''
+    if to_drop:
+        return df.drop(col_names,axis=1)
+    return df[col_names]
+
+def dummify(df, cols, constant_and_drop=False):
+    '''
+        Given a dataframe, for all the columns which are not numericly typed already,
+        create dummies. This will NOT remove one of the dummies which is required for
+        logistic regression (in such case, constant_and_drop should be True).
+        To run outside of class
+    '''
+    df = pd.get_dummies(df, columns=cols, drop_first=constant_and_drop)
+    if constant_and_drop:
+        const = np.full(len(df), 1)
+        df['constant'] = const
+    return df
