976461a69f7211544b5dbb9b9c22958d7b1c5f31
rita.glv@gmail.com <johndoe@example.com>
2017-05-03 14:19:31 -0700

    consolidate app and predict

--- src/app.py
+++ src/app.py
@@ -23,9 +23,6 @@ def index():
     # return "boo"
     return render_template('index.html')
 
-@app.route('/submit')
-def submit():
-    return render_template('submit.html')
 
 @app.route('/score', methods=['GET','POST'])
 def score():
@@ -38,13 +35,6 @@ def score():
     db.save_to_db(X,y)
     return render_template('/show_json.html', table=X.to_html())
 
-@app.route('/predict', methods=['POST'])
-def predict():
-
-    X = request.form['body']
-    y_label = model.predict([X])
-
-    return render_template('/predict.html', X=X, y_label=y_label[0])
 
 @app.route('/dashboard')
 def dashboard():
@@ -53,18 +43,6 @@ def dashboard():
 
     history = df[['name','fraud']]
     return render_template('/dashboard.html', table=history.to_html())
-
-
-def load_data(sc):
-
-    d = requests.get(url).json()
-    X = pd.DataFrame.from_dict(d, orient='index').T
-    # y = model.predict(X)
-    y = True
-
-    db.save_to_db(X,y)
-
-    s.enter(30, 1, load_data, (sc,))
 
 
 if __name__ == '__main__':
